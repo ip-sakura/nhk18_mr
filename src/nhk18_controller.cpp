8b135d599e07b482e373592242144ac2dfb307a3
syatch <32508310+syatch@users.noreply.github.com>
2018-04-16 21:11:59 +0900

    by pi

--- src/nhk18_controller.cpp
+++ src/nhk18_controller.cpp
@@ -17,6 +17,10 @@
 //ステピ状態
 #define STPUP 1
 #define STPDW 2
+//servo
+/*#define HIDE_BOTH 0
+#define SHOW_RED 1
+#define SHOW_BLUE 2*/
 
 //エンコーダ用
 #define DELTA_L 0
@@ -24,13 +28,16 @@
 #define DGAP 2
 
 #define THRESHOLD 0.1
+#define R_STICK_THRESH 0.5
 #define TOPPOWER 80
 #define MINPOWER 50
 
-#define L_OR_R_PLUS 5//Lbutton Rbutton ni taiou addition
+#define L_OR_R_PLUS 10//Lbutton Rbutton ni taiou addition
+#define L_OR_R_MINUS 13
 
-#define STP0 10 //ステピ初期位置
-#define STPX 150 - STP0//ラックを持ち上げる際の変位（mm）
+#define STP0 20 //ステピ初期位置
+#define STP1 0 // after STP0
+#define STPX 157//floor to X postion（mm）
 
 #define SUPRESS 0.1//使わない
 #define SUPRESS_COR 0.5 //補正用サプレッサー 使わない
@@ -40,7 +47,7 @@ int status = STOP;//状態
 int status_buf = STOP;//直前の状態
 int stp_status = STOP;//ステピの状態
 int stp_status_buf = STOP;//直前のステピの状態
-int r_ispushed,l_ispushed,a_ispushed;
+int r_ispushed,l_ispushed,a_ispushed,l2_ispushed,r2_ispushed;
 int sw = 0;
 int span_ms = 1;//速度？積算のタイムスパン
 int cnt = 0;//タイムスパン用カウンタ
@@ -51,97 +58,32 @@ int motorpw_l=0;
 int motorpw_r=0;
 int extra_correction = 0;//補正用
 float gap_ratio = 0;//エンコーダからとってきた差を割合で表す。
-bool vlr_max = 0; //=DELTA_L(0) or DELTA_R(1)で考える
+bool vlr_max = 0; //=DELTA_L(0) or DELTA_R(1)で考える 
 
 std_msgs::Int16 mpwsender_l,mpwsender_r;//最終的にpubされるmotorpw
 std_msgs::Int16 stpsender_a,stpsender_b;
 std_msgs::Float64 ex;
+//std_msgs::Int16 servo_sender;
+//ros::Publisher servo_pub;
+std_msgs::Int16 stp_join;
+ros::Publisher stp_join_pub;
 
-void set_motor_speed(int& motor_pw,int target_pw){//PID制御でmotorpwに積算する
-  
-  /* int gap = delta * (target_pw - motor_pw);
-  if(gap == 0){
-    if(target_pw-motor_pw > 0)gap = 1;
-    else if (target_pw-motor_pw < 0)gap = -1;
-    else gap = 0;
-  }
-  
-  motor_pw += gap;*/
-}
-
-void get_correction(){
-  /*if(motorpw_l>0){
-    if(gap_ratio > 0){
-      extra_correction--;
-    }else{
-      extra_correction++;
-    }
-  }else if(motorpw_l<0){
-    if(gap_ratio > 0){
-      extra_correction++;
-    }else{
-      extra_correction--;
-    }
-    }*/
-  
-    /*if(vlr_max == DELTA_L){
-      //set_correction(extra_correction,-(int)(motorpw_l * gap_ratio));
-      extra_correction--;
-    }else if(vlr_max == DELTA_R){
-      //set_correction(extra_correction,(int)motorpw_r * gap_ratio);
-      extra_correction++;
-      }*/
-    
-  
-
-  /*if(gap_ratio > 0){
-    if(mpwsender_l.data > 0 && mpwsender_r.data > 0){
-      if(extra_correction < 0)extra_correction = 0;
-      extra_correction += 1;
-    }else if(mpwsender_l.data < 0 && mpwsender_r.data < 0){
-      if(extra_correction > 0)extra_correction = 0;
-      extra_correction -= 1;
-    }
-    }*/
-}
 
 void set_stp_move(){//mm単位でどれだけ回すかmsgsに格納
   if(stp_status == STPUP){
-    stpsender_a.data = STPX;
+    stpsender_a.data = STPX-STP0 - STP1;
     stpsender_b.data = STPX;
     ROS_INFO("stp cw \n");
   }else if(stp_status == STPDW){
-    stpsender_a.data = -STPX;
+    stpsender_a.data = -STPX + STP0 + STP1;
     stpsender_b.data = -STPX;
     ROS_INFO("stp ccw \n");
   }//else if(stp_status == STOP){
   //stpsender_a.data = 0;
   //stpsender_b.data = 0;
   //ROS_INFO("stp stop");
-
-  if(a_ispushed){
-    stpsender_a.data = STP0;
-    a_ispushed = 0;
-  }
 }
 
-
-void set_motor_status(){//statusに応じてmotorpwを変化させる
-  /*if(cnt == span_ms){
-    if(status == FORWARD){
-      set_motor_speed(motorpw_l,TOPPOWER);
-      set_motor_speed(motorpw_r,TOPPOWER);
-    }else if(status == BACK){
-      set_motor_speed(motorpw_l,-TOPPOWER);
-      set_motor_speed(motorpw_r,-TOPPOWER);
-    }else if(status == STOP){
-      set_motor_speed(motorpw_l,0);
-      set_motor_speed(motorpw_r,0);
-    }
-    cnt = 0;
-  }
-  cnt++;*/
-}
 
 void joyCallback(const sensor_msgs::Joy::ConstPtr& joy){
   if(joy->axes[1] >= THRESHOLD){
@@ -156,7 +98,7 @@ void joyCallback(const sensor_msgs::Joy::ConstPtr& joy){
     status = STOP;
     motorpw_l = 0;
     motorpw_r = 0;
-  }
+}
 
   if(joy->buttons[5]){//Rボタン（右寄り走行）
     r_ispushed = 1;
@@ -170,6 +112,17 @@ void joyCallback(const sensor_msgs::Joy::ConstPtr& joy){
     cnt = 0;
   }
 
+  if(joy->buttons[6]){
+    if(motorpw_l >= 0){
+      motorpw_l += L_OR_R_PLUS * 2;
+    }else{
+      motorpw_l -= L_OR_R_PLUS * 2;
+    }
+  }else if(joy->buttons[7]){
+    if(motorpw_r >= 0)motorpw_r += L_OR_R_MINUS * 2;
+    else motorpw_r -= L_OR_R_MINUS * 2;
+  }
+
   if(joy -> axes[5] >= THRESHOLD){//十字キー上下でステピ動かす予定
     stp_status = STPUP;
   }else if(joy -> axes[5] <= -THRESHOLD){
@@ -180,8 +133,27 @@ void joyCallback(const sensor_msgs::Joy::ConstPtr& joy){
 
   if(joy->buttons[0]){
     a_ispushed = 1;
-  }
+    stpsender_a.data = STP0;
+}else if(joy->buttons[2]){
+    stp_status = STOP;
+    stpsender_a.data = 0;
+  }else if(joy->buttons[1]){
+    a_ispushed =1;
+    stpsender_a.data = STP1;
+}else if(joy->buttons[3]){
+    stp_join.data++;
+    if(stp_join.data == 3)stp_join.data = 0;
+    stp_join_pub.publish(stp_join);
+}
     
+  
+  if(joy->axes[2] >= R_STICK_THRESH){
+    motorpw_r = 10;
+    motorpw_l = -10;
+  }else if(joy->axes[2] <= -R_STICK_THRESH){
+    motorpw_l = 10;
+    motorpw_r = -10;
+  }
 }
 
 int max(int l, int r){
@@ -205,19 +177,6 @@ int max(int l, int r){
 }
 
 void encCallback(const std_msgs::Int32MultiArray& lmr){
-  /*int vl = lmr.data[DELTA_L];
-  int vr = lmr.data[DELTA_R];
-  int gap = lmr.data[DGAP];
-
-  //ROS_INFO("encCallback");
-
-  if(vl == 0 || vr == 0){
-    gap_ratio = 0;
-    extra_correction = 0;
-  }else gap_ratio = (float)gap / (float)max(vl,vr);
-  
-  //if(gap_ratio < 0)gap_ratio *= -1;
-  ex.data = extra_correction;*/
 }
 
 int main (int argc, char **argv){
@@ -229,8 +188,9 @@ int main (int argc, char **argv){
   ros::Subscriber v_lmr = nh.subscribe("enc",1000, encCallback);
   ros::Publisher mr_pub = nh.advertise<std_msgs::Int16>("mr",1000);
   ros::Publisher ml_pub = nh.advertise<std_msgs::Int16>("ml",1000);
+  stp_join_pub = nh.advertise<std_msgs::Int16>("stp_join",1000);
   ros::Publisher stpa_pub = nh.advertise<std_msgs::Int16>("stpa",1000);
-  ros::Publisher stpb_pub = nh.advertise<std_msgs::Int16>("stpb",1000);
+  ros::Publisher stp_join_pub = nh.advertise<std_msgs::Int16>("stp_join",1000);
   ros::Rate loop_rate(50);
 
   ros::Publisher extra = nh.advertise<std_msgs::Float64>("ex",1000);
@@ -242,20 +202,15 @@ int main (int argc, char **argv){
 
     //get_correction();
     extra.publish(ex);
-
-    //mpwsender_l.data += extra_correction;
-
-    //if(vlr_max == DELTA_L)mpwsender_r.data += extra_correction;//補正
-    //else if (vlr_max == DELTA_R)mpwsender_l.data += extra_correction;
     
     if(l_ispushed == 0 && r_ispushed == 1){//L,Rボタンに応じて片方に寄る走行をさせる
       if(mpwsender_r.data >= 0)
-	mpwsender_r.data += L_OR_R_PLUS;//(mpwsender_l.data * SUPRESS);
-      else mpwsender_r.data -= L_OR_R_PLUS;//(mpwsender_l.data * SUPRESS);
+	mpwsender_r.data += L_OR_R_PLUS;
+      else mpwsender_r.data -= L_OR_R_PLUS;
     }else if(l_ispushed == 1 && r_ispushed == 0){
       if(mpwsender_l.data >= 0)
-	mpwsender_l.data += L_OR_R_PLUS;//(mpwsender_r.data * SUPRESS);
-      else mpwsender_l.data -= L_OR_R_PLUS * 3;//(mpwsender_r.data * SUPRESS);
+	mpwsender_l.data += L_OR_R_PLUS;
+      else mpwsender_l.data -= L_OR_R_MINUS;
     }
 
     mr_pub.publish(mpwsender_r);
@@ -269,7 +224,11 @@ int main (int argc, char **argv){
       stp_status_buf = stp_status;
       set_stp_move();
       stpa_pub.publish(stpsender_a);
-      stpb_pub.publish(stpsender_b);
+    }
+
+    if(a_ispushed){
+      stpa_pub.publish(stpsender_a);
+      a_ispushed = 0;
     }
 
     ros::spinOnce();
